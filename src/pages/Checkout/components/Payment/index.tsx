9f1f033f87c4b0de0b0214cc010a2f14ffb2cd86
Bruno Fialho <brugerf@gmail.com>
2022-09-30 13:43:05 -0300

    feat: payment component

--- src/pages/Checkout/components/Payment/index.tsx
+++ src/pages/Checkout/components/Payment/index.tsx
@@ -1,5 +1,35 @@
-import { PaymentContainer } from './styles'
+import { Bank, CreditCard, CurrencyDollar, Money } from 'phosphor-react'
+import { InputContainer, PaymentContainer, PaymentHeader } from './styles'
 
 export function Payment() {
-  return <PaymentContainer></PaymentContainer>
+  return (
+    <PaymentContainer>
+      <PaymentHeader>
+        <CurrencyDollar />
+        <div>
+          <p>Pagamento</p>
+          <p>
+            O pagamento é feito na entrega. Escolha a forma que deseja pagar
+          </p>
+        </div>
+      </PaymentHeader>
+      <InputContainer>
+        <label>
+          <input type="radio" name="options" id="credit" />
+          <CreditCard />
+          Cartão de Crédito
+        </label>
+        <label>
+          <input type="radio" name="options" id="debit" />
+          <Bank />
+          Cartão de Débito
+        </label>
+        <label>
+          <input type="radio" name="options" id="money" />
+          <Money />
+          Dinheiro
+        </label>
+      </InputContainer>
+    </PaymentContainer>
+  )
 }
--- src/pages/Checkout/components/Payment/styles.ts
+++ src/pages/Checkout/components/Payment/styles.ts
@@ -7,4 +7,102 @@ export const PaymentContainer = styled.div`
   padding: 2.5rem;
 
   background-color: ${(props) => props.theme['base-card']};
+
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`
+
+export const PaymentHeader = styled.header`
+  width: 100%;
+  min-height: 2.75rem;
+
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-between;
+  gap: 0.5rem;
+
+  svg {
+    width: 1.375rem;
+    height: 1.375rem;
+    color: ${(props) => props.theme.purple};
+  }
+
+  div {
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    gap: 0.125rem;
+
+    p:first-of-type {
+      font-size: 1rem;
+      color: ${(props) => props.theme['base-subtitle']};
+    }
+
+    p:last-of-type {
+      font-size: 0.875rem;
+      color: ${(props) => props.theme['base-text']};
+    }
+  }
+`
+
+export const InputContainer = styled.div`
+  width: 100%;
+  min-height: 3.125rem;
+
+  display: flex;
+  flex-direction: row;
+  gap: 0.75rem;
+
+  label {
+    width: 100%;
+    height: 100%;
+    padding: 1rem;
+    border-radius: 6px;
+    border: 1px solid transparent;
+    cursor: pointer;
+
+    color: ${(props) => props.theme['base-text']};
+    background-color: ${(props) => props.theme['base-button']};
+
+    font-size: 0.75rem;
+    text-transform: uppercase;
+    line-height: 1.6;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 0.75rem;
+
+    svg {
+      width: 1rem;
+      height: 1rem;
+      color: ${(props) => props.theme.purple};
+    }
+
+    input {
+      display: none;
+    }
+  }
+
+  label:hover {
+    background-color: ${(props) => props.theme['base-hover']};
+  }
+
+  label:has(input:checked) {
+    background-color: ${(props) => props.theme['purple-light']};
+    border: 1px solid ${(props) => props.theme.purple};
+  }
+
+  @media (max-width: 576px) {
+    flex-direction: column;
+    align-items: center;
+
+    label {
+      max-width: 50%;
+    }
+  }
 `
